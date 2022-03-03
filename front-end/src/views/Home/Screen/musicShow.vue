2ff5ca85a95a10ff9c2301b3d5018d9077e7aad7
kangzhongqi <73341980+kangzhongqi@users.noreply.github.com>
2022-03-03 12:56:47 +0800

    add arrows

--- front-end/src/views/Home/Screen/musicShow.vue
+++ front-end/src/views/Home/Screen/musicShow.vue
@@ -10,16 +10,28 @@
 <!--  </el-table>-->
 
   <!--轮播图-->
-  <el-carousel height="85vh" direction="vertical" :autoplay="false" arrow="hover" loop="false" trigger="click">
+  <el-carousel ref="nop" height="85vh" direction="vertical" :autoplay="false" arrow="always" loop="false" trigger="click">
     <el-carousel-item v-for="item in 16" :key="item">
-      <h3>{{ item }}</h3>
-    </el-carousel-item>
+      <div class="btndiv">
+        <div id="btn_up" @click="this.$refs.nop.prev();"><img height="70" src="../../../assets/up.png"></div>
+              <h3>{{ item }}</h3>
+        <div id="btn_down" @click="this.$refs.nop.next();"><img height="70" src="../../../assets/down.png"></div>
+      </div>
+
+</el-carousel-item>
   </el-carousel>
 </template>
 
 <script setup>
-// import { ref } from 'vue'
-// const loading = ref(true)
+// export default {
+//   data() {
+//     return {
+//         upUrl:require("../../../assets/up.png")
+//      }
+// }
+//   // require upUrl from
+// }
+
 </script>
 
 <style scoped>
@@ -28,7 +40,7 @@
   font-size: 18px;
   opacity: 0.75;
   line-height: 300px;
-  height: 85vh;
+  /*height: 85vh;*/
   margin: 0;
   text-align: center;
 }
@@ -39,5 +51,14 @@
 
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
+}
+
+#btn_up, #btn_down {
+  text-align: center;
+  opacity: 0.75;
+
+}
+#btn_down {
+  margin: 20vh;
 }
 </style>
