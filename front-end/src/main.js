cf32f95dad593a60dbf4721d72827261e5e8d470
kangzhongqi <73341980+kangzhongqi@users.noreply.github.com>
2022-03-13 16:34:16 +0800

    built the connection between the front-end and the back-end

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
-
+import 'bootstrap/dist/css/bootstrap.css';
 // 引入EmelemtPlus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
@@ -10,5 +10,4 @@ import 'element-plus/dist/index.css'
 // 环境安装: npm install vue-particles --save-dev
 // Reference: https://blog.csdn.net/weixin_42201180/article/details/111941697
 import VueParticles from 'vue-particles'
-
 createApp(App).use(router).use(ElementPlus).use(VueParticles).mount('#app')
