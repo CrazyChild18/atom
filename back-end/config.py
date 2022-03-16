ec074ce51772642c0b4f3aeb508012bead7685aa
kangzhongqi <73341980+kangzhongqi@users.noreply.github.com>
2022-03-16 16:24:05 +0800

    部分前后端交互配置

---
+++ back-end/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'), encoding='utf-8')
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'atom.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
